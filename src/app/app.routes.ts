985bf5500a7690e36a6ea470bb41da27eafc1b6b
Mateo Mora <mateomora2015@gmial.com>
2024-06-22 00:42:09 -0500

    Mi primer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import path from 'node:path';
+import { VistaComponent } from './vista/vista.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+
+    {path:'rondas-tecnologia', component: VistaComponent}
+];
