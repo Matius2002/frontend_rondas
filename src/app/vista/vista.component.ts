8dcea646e436e3b718ffc07f560c1e5c459f0ccc
Mateo Mora <mateomora2015@gmial.com>
2024-07-03 16:45:43 -0500

    Cambios realizados en Tailwind

--- src/app/vista/vista.component.ts
+++ src/app/vista/vista.component.ts
@@ -8,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrl: './vista.component.css'
 })
 export class VistaComponent {
+  files: File[] = [];
+  imageUrls: string[] = [];
+  readonly maxFiles: number = 5; // Máximo número de archivos permitidos
 
+  updateFileCount(event: Event): void {
+    const element = event.target as HTMLInputElement;
+    const files = Array.from(element.files ?? []);
+    if (files.length > this.maxFiles) {
+      alert(`Solo puede seleccionar un máximo de ${this.maxFiles} imágenes.`);
+      element.value = ''; // Resetea el campo de archivo
+      this.files = [];
+      this.imageUrls = [];
+    } else {
+      this.files = files;
+      this.updateImagePreviews();
+    }
+  }
+
+  updateImagePreviews(): void {
+    this.imageUrls = this.files.map(file => URL.createObjectURL(file));
+  }
+
+  calculateProgress(): string {
+    return `${(this.files.length / this.maxFiles) * 100}%`;
+  }
 }
